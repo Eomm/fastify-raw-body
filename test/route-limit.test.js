9bcc90c7113b885879d1a4975b4f81f735e279b5
Manuel Spigolon <behemoth89@gmail.com>
2022-10-25 19:36:20 +0200

    test: improve

--- test/route-limit.test.js
+++ test/route-limit.test.js
@@ -4,10 +4,22 @@ const t = require('tap')
 const Fastify = require('fastify')
 const rawBody = require('../plugin')
 
-t.test('body limit per route', async t => {
-  const app = Fastify({ bodyLimit: 5 })
+const payloadMini = { h: 1 }
+const payloadSmall = { hello: '1' }
+const payloadBig = { hello: '1'.repeat(100) }
 
-  const payload = { hello: '123456789012345678901234567890' }
+const limitError = Object.freeze({
+  statusCode: 413,
+  code: 'FST_ERR_CTP_BODY_TOO_LARGE',
+  error: 'Payload Too Large',
+  message: 'Request body is too large'
+})
+
+async function buildApp ({
+  serverLimit,
+  routeLimit
+}) {
+  const app = Fastify({ bodyLimit: serverLimit })
 
   await app.register(rawBody, {
     field: 'rawBody',
@@ -15,71 +27,118 @@ t.test('body limit per route', async t => {
     runFirst: false
   })
 
-  app.post('/100', {
+  app.post('/route-limit', {
     config: { rawBody: true },
-    bodyLimit: 100
-  }, (req, reply) => {
-    t.pass('body is ok')
-    return req.rawBody
-  })
-
-  app.post('/50', {
-    config: {
-      rawBody: true
-    },
-    bodyLimit: 41
-  }, (req, reply) => {
-    t.fail('body is not ok')
+    bodyLimit: routeLimit,
+    handler (req) { return req.rawBody }
   })
 
   app.post('/server-limit', {
-    config: {
-      rawBody: true
-    }
-  }, (req, reply) => {
-    t.fail('body is not ok')
+    config: { rawBody: true },
+    handler (req) { return req.rawBody }
   })
 
-  await t.test('must not throw if body is smaller than limit', async t => {
+  return app
+}
+
+t.test('body limit per route (route-limit > server-limit)', async t => {
+  const app = await buildApp({
+    serverLimit: 10,
+    routeLimit: 100
+  })
+
+  await t.test('must succeed if body is smaller than route limit', async t => {
     const res = await app.inject({
       method: 'POST',
-      url: '/100',
-      payload
+      url: '/route-limit',
+      payload: payloadSmall
     })
 
     t.equal(res.statusCode, 200)
-    t.equal(res.payload, JSON.stringify(payload))
+    t.equal(res.payload, JSON.stringify(payloadSmall))
   })
 
-  await t.test('must reject if body is bigger than limit', async t => {
+  await t.test('must reject if body is bigger than route limit', async t => {
     const res = await app.inject({
       method: 'POST',
-      url: '/50',
-      payload
+      url: '/route-limit',
+      payload: payloadBig
     })
 
     t.equal(res.statusCode, 413)
-    t.same(res.json(), {
-      statusCode: 413,
-      code: 'FST_ERR_CTP_BODY_TOO_LARGE',
-      error: 'Payload Too Large',
-      message: 'Request body is too large'
+    t.same(res.json(), limitError)
+  })
+
+  await t.test('must succeed if body is smaller then server limit', async t => {
+    const res = await app.inject({
+      method: 'POST',
+      url: '/server-limit',
+      payload: payloadMini
     })
+
+    t.equal(res.statusCode, 200)
+    t.equal(res.payload, JSON.stringify(payloadMini))
   })
 
   await t.test('must reject if body is bigger then the server limit', async t => {
     const res = await app.inject({
       method: 'POST',
       url: '/server-limit',
-      payload: { hello: '1' }
+      payload: payloadSmall
     })
 
     t.equal(res.statusCode, 413)
-    t.same(res.json(), {
-      statusCode: 413,
-      code: 'FST_ERR_CTP_BODY_TOO_LARGE',
-      error: 'Payload Too Large',
-      message: 'Request body is too large'
+    t.same(res.json(), limitError)
+  })
+})
+
+t.test('body limit per route (route-limit < server-limit)', async t => {
+  const app = await buildApp({
+    serverLimit: 100,
+    routeLimit: 10
+  })
+
+  await t.test('must succeed if body is smaller than route limit', async t => {
+    const res = await app.inject({
+      method: 'POST',
+      url: '/route-limit',
+      payload: payloadMini
     })
+
+    t.equal(res.statusCode, 200)
+    t.equal(res.payload, JSON.stringify(payloadMini))
+  })
+
+  await t.test('must reject if body is bigger than route limit', async t => {
+    const res = await app.inject({
+      method: 'POST',
+      url: '/route-limit',
+      payload: payloadSmall
+    })
+
+    t.equal(res.statusCode, 413)
+    t.same(res.json(), limitError)
+  })
+
+  await t.test('must succeed if body is smaller then server limit', async t => {
+    const res = await app.inject({
+      method: 'POST',
+      url: '/server-limit',
+      payload: payloadSmall
+    })
+
+    t.equal(res.statusCode, 200)
+    t.equal(res.payload, JSON.stringify(payloadSmall))
+  })
+
+  await t.test('must reject if body is bigger then the server limit', async t => {
+    const res = await app.inject({
+      method: 'POST',
+      url: '/server-limit',
+      payload: payloadBig
+    })
+
+    t.equal(res.statusCode, 413)
+    t.same(res.json(), limitError)
   })
 })
